a96ee373d37c13fb53166b1b7c55dfb67381fb21
ssook7979 <ssook7979@gmail.com>
2022-10-19 23:53:13 +0900

    added CSP class

--- constraint-satisfaction-problem/csp.py
+++ constraint-satisfaction-problem/csp.py
@@ -11,3 +11,26 @@ class Constraint(Generic[V, D], ABC):
     @abstractmethod
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         pass
+
+class CSP(Generic[V, D]):
+    def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
+        self.variables: List[V] = variables # 제약조건을 확인할 변수
+        self.domains: Dict[V, List[D]] = domains
+        self.constraints: Dict[V, List[Constraint[V, D]]] = {}
+        for variable in self.variables:
+            self.constraints[variable] = []
+            if variable not in self.domains:
+                raise LookupError("모든 변수에 도메인이 할당되어야 합니다.")
+    
+    def add_constraint(self, constraint: Constraint[V, D]) -> None:
+        for variable in constraint.variables:
+            if variable not in self.variables:
+                raise LookupError("제약 조건 변수가 아닙니다.")
+            else:
+                self.constraints[variable].append(constraint)
+
+    def consistent(self, variable: V, assignment: Dict[V, D]) -> bool:
+        for constraint in self.constraints[variable]:
+            if not constraint.satisfied(assignment):
+                return False
+        return True
