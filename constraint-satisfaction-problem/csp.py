1e4a3792449d7628abe0738612325786cc66e47b
ssook7979 <ssook7979@gmail.com>
2022-10-20 23:51:46 +0900

    added backtracking method

--- constraint-satisfaction-problem/csp.py
+++ constraint-satisfaction-problem/csp.py
@@ -34,3 +34,20 @@ class CSP(Generic[V, D]):
             if not constraint.satisfied(assignment):
                 return False
         return True
+    
+    def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+        if len(assignment) == len(self.variables):
+            return assignment
+        
+        unassigned: List[V] = [v for v in self.variables if v not in assignment]
+
+        first: V = unassigned[0]
+        for value in self.domains[first]:
+            local_assignment = assignment.copy()
+            local_assignment[first] = value
+            if self.consistent(first, local_assignment):
+                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
+
+                if result is not None:
+                    return result
+        return None
