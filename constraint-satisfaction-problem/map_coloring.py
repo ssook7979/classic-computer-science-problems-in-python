26ead4d25bdca121fbb43b2544977b5c5b5a4584
ssook7979 <ssook7979@gmail.com>
2022-10-21 23:55:35 +0900

    added implementation of constraint for solving map coloring prob

---
+++ constraint-satisfaction-problem/map_coloring.py
@@ -0,0 +1,13 @@
+from csp import Constraint, CSP
+from typing import Dict, List, Optional
+
+class MamColoringConstraint(Constraint[str, str]):
+    def __init__(self, place1: str, place2: str) -> None:
+        super().__init__([place1, place2])
+        self.place1: str = place1
+        self.place2: str = place2
+
+    def satisfied(self, assignment: Dict[str, str]) -> bool:
+        if self.place1 not in assignment or self.place2 not in assignment:
+            return True
+        return assignment[self.place1] != assignment[self.place2]
