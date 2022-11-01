4c4c7a1be5874c50a976fa533e3dad19ee939f04
ssook7979 <ssook7979@gmail.com>
2022-11-01 23:58:48 +0900

    added test data

--- constraint-satisfaction-problem/map_coloring.py
+++ constraint-satisfaction-problem/map_coloring.py
@@ -11,3 +11,9 @@ class MamColoringConstraint(Constraint[str, str]):
         if self.place1 not in assignment or self.place2 not in assignment:
             return True
         return assignment[self.place1] != assignment[self.place2]
+
+if __name__ == "__main__":
+    variables: List[str] = ["웨스턴 오스트레일리아", "노던 준주", "사우스 오스트레일리아", "퀸즐랜드", "뉴사우스웨일스", "빅토리아", "태즈메이니아"]
+    domains: Dict[str, List[str]] = {}
+    for variable in variables:
+        domains[variable] = ["빨강", "처럭", "파랑"]
