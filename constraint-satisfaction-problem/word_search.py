f77ba803f371570026f7daa094287be687b7518d
ssook7979 <ssook7979@gmail.com>
2022-11-02 23:56:50 +0900

    added class and functions related to Grid

---
+++ constraint-satisfaction-problem/word_search.py
@@ -0,0 +1,17 @@
+from typing import NamedTuple, List, Dict, Optional
+from random import choice
+from string import ascii_uppercase
+from csp import CSP, Constraint
+
+Grid = List[List[str]]
+
+class Gridlocation(NamedTuple):
+    row: int
+    column: int
+
+def generate_grid(rows: int, columns: int):
+    return [[choice(ascii_uppercase) for c in range(columns)] for r in range(rows)]
+
+def display_grid(grid: Grid) -> None:
+    for row in grid:
+        print("".join(row))
