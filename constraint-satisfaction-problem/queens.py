1dce96e3d658537bfa398ed6f4ceaccb394f5dea
hspark <ssook7979@gmail.com>
2022-11-09 18:55:21 +0900

    move codes to queens file

--- constraint-satisfaction-problem/queens.py
+++ constraint-satisfaction-problem/queens.py
@@ -1,12 +1,15 @@
+from typing import List, Dict, Optional
 from csp import Constraint, CSP
-from typing import Dict, List, Optional
+# variables = the column of the queen in question
+# domains = the possible rows(1 through 8)
+
 
 class QueensConstraint(Constraint[int, int]):
-    def __init__(self, columns: List[V]) -> None:
+    def __init__(self, columns: List[int]) -> None:
         super().__init__(columns)
-        self.comlumns: List[int] = columns
-    
-    def satisfied(self, assignment: Dict[int, int]) -> bool:
+        self.columns: List[int] = columns
+
+    def satisfied(self, assignment: Dict[int, int] = {}):
         for q1c, q1r in assignment.items():
             for q2c in range(q1c + 1, len(self.columns) + 1):
                 if q2c in assignment:
@@ -17,15 +20,16 @@ class QueensConstraint(Constraint[int, int]):
                         return False
         return True
 
+
 if __name__ == "__main__":
     columns: List[int] = [1, 2, 3, 4, 5, 6, 7, 8]
     rows: Dict[int, List[int]] = {}
     for column in columns:
         rows[column] = [1, 2, 3, 4, 5, 6, 7, 8]
-    csp : CSP[int, int] = CSP(columns, rows)
+    csp: CSP[int, int] = CSP(columns, rows)
     csp.add_constraint(QueensConstraint(columns))
     solution: Optional[Dict[int, int]] = csp.backtracking_search()
     if solution is None:
-        print("답을 찾을 수 없습니다.")
+        print("No solution found!")
     else:
         print(solution)
