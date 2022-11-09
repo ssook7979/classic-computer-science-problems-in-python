b035c3f2920020860eb2d7ff047aee3d5238b433
hspark <ssook7979@gmail.com>
2022-11-09 18:00:22 +0900

    added descriptions

--- constraint-satisfaction-problem/csp.py
+++ constraint-satisfaction-problem/csp.py
@@ -1,8 +1,9 @@
 from typing import Generic, TypeVar, Dict, List, Optional
 from abc import ABC, abstractmethod
 
-V = TypeVar('V') # 변수 Variable 타입
-D = TypeVar('D') # 도메인 Domain 타입
+V = TypeVar('V')  # 변수 Variable 타입
+D = TypeVar('D')  # 도메인 Domain 타입
+
 
 class Constraint(Generic[V, D], ABC):
     def __init__(self, variables: List[V]) -> None:
@@ -12,20 +13,25 @@ class Constraint(Generic[V, D], ABC):
     def satisfied(self, assignment: Dict[V, D]) -> bool:
         pass
 
+# the gathering point for v, d, c
+
+
 class CSP(Generic[V, D]):
     def __init__(self, variables: List[V], domains: Dict[V, List[D]]) -> None:
-        self.variables: List[V] = variables # 제약조건을 확인할 변수
+        self.variables: List[V] = variables  # variables to be constrained
+        # domain of each variable(dict mapping variables to lists of possible values)
         self.domains: Dict[V, List[D]] = domains
         self.constraints: Dict[V, List[Constraint[V, D]]] = {}
         for variable in self.variables:
             self.constraints[variable] = []
             if variable not in self.domains:
-                raise LookupError("모든 변수에 도메인이 할당되어야 합니다.")
-    
+                raise LookupError(
+                    "Every variable should hav a domain assigned to it.")
+
     def add_constraint(self, constraint: Constraint[V, D]) -> None:
         for variable in constraint.variables:
             if variable not in self.variables:
-                raise LookupError("제약 조건 변수가 아닙니다.")
+                raise LookupError("Variable in constraint not in CSP")
             else:
                 self.constraints[variable].append(constraint)
 
@@ -34,20 +40,28 @@ class CSP(Generic[V, D]):
             if not constraint.satisfied(assignment):
                 return False
         return True
-    
+
+    # a kind of recursive depth-first search
     def backtracking_search(self, assignment: Dict[V, D] = {}) -> Optional[Dict[V, D]]:
+        # the base case for the recursive search
         if len(assignment) == len(self.variables):
             return assignment
-        
-        unassigned: List[V] = [v for v in self.variables if v not in assignment]
 
+        # get all variables in the CSP but not in the assignment
+        unassigned: List[V] = [
+            v for v in self.variables if v not in assignment]
+        # get every possible domain value of the first unassigned variable
         first: V = unassigned[0]
+        # try assigning all possible domain values for that bariable, one at a time
+        # the new assignment is stored in local assignment
         for value in self.domains[first]:
             local_assignment = assignment.copy()
             local_assignment[first] = value
+            # if the new assignment is consistent with all of the constrains then continue recursively search
             if self.consistent(first, local_assignment):
-                result: Optional[Dict[V, D]] = self.backtracking_search(local_assignment)
-
+                result: Optional[Dict[V, D]] = self.backtracking_search(
+                    local_assignment)
+                # if we didn't find the result, we will end up backtracking
                 if result is not None:
                     return result
         return None
