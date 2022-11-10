a22371b1cd925ec4c8b344cadc34ef137a09c727
hspark <ssook7979@gmail.com>
2022-11-10 15:19:18 +0900

    added comments

--- constraint-satisfaction-problem/word_search.py
+++ constraint-satisfaction-problem/word_search.py
@@ -5,16 +5,20 @@ from csp import CSP, Constraint
 
 Grid = List[List[str]]
 
+
 class GridLocation(NamedTuple):
     row: int
     column: int
 
+
 def generate_grid(rows: int, columns: int):
     return [[choice(ascii_uppercase) for c in range(columns)] for r in range(rows)]
 
+
 def display_grid(grid: Grid) -> None:
     for row in grid:
-        print("".join(row)) 
+        print("".join(row))
+
 
 def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
     domain: List[List[GridLocation]] = []
@@ -26,11 +30,17 @@ def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
             columns: range = range(col, col+length + 1)
             rows: range = range(col, col + 1)
             if col + length <= width:
+                # left to right
                 domain.append([GridLocation(row, c) for c in columns])
+                # diagonal towards bottom right
+                if row + length <= height:
+                    domain.append([GridLocation(r, col + (r - row))
+                                  for r in rows])
             if row + length <= height:
-                domain.append([GridLocation(r, col + (r - row)) for r in rows])
-            if row + length <= height:
+                # top to bottom
                 domain.append([GridLocation(r, col) for r in rows])
+                # diagonal towards bottom left
                 if col - length >= 0:
-                    domain.append([GridLocation(r, col - (r - row)) for r in rows])
+                    domain.append([GridLocation(r, col - (r - row))
+                                  for r in rows])
     return domain
