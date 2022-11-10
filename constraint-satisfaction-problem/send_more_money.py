02d651471308b5a953063a001f944e61d83cb50e
hspark <ssook7979@gmail.com>
2022-11-10 16:49:22 +0900

    added send more money prob

---
+++ constraint-satisfaction-problem/send_more_money.py
@@ -0,0 +1,44 @@
+from csp import Constraint, CSP
+from typing import List, Dict, Optional
+
+
+class SendMoreMoneyConstraint(Constraint[str, int]):
+    def __init__(self, letters: List[str]) -> None:
+        super().__init__(letters)
+        self.letters: List[str] = letters
+
+    def satisfied(self, assignment: Dict[str, int]) -> bool:
+        # if there are duplicate values, then it's not a solution
+        if len(set(assignment.values())) < len(assignment):
+            return False
+
+        # if all variables have been assigned, check if it adds correctly
+        if len(assignment) == len(self.letters):
+            s: int = assignment["S"]
+            e: int = assignment["E"]
+            n: int = assignment["N"]
+            d: int = assignment["D"]
+            m: int = assignment["M"]
+            o: int = assignment["O"]
+            r: int = assignment["R"]
+            y: int = assignment["Y"]
+            send: int = s * 1000 + e * 100 + n * 10 + d
+            more: int = n * 1000 + o * 100 + r * 10 + e
+            money: int = m * 10000 + o * 1000 + n * 100 + e * 10 + y
+            return send + more == money
+        return True  # no conflict # if all letters have not yet been assigned. to ensure that a partial solution continuew to be worked on
+
+
+if __name__ == "__main__":
+    letters: List[str] = ["S", "E", "N", "D", "M", "O", "R", "Y"]
+    possible_digits: Dict[str, List[int]] = {}
+    for letter in letters:
+        possible_digits[letter] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    possible_digits["M"] = [1]
+    csp: CSP[str, int] = CSP(letters, possible_digits)
+    csp.add_constraint(SendMoreMoneyConstraint(letters))
+    solution: Optional[Dict[str, int]] = csp.backtracking_search()
+    if solution is None:
+        print("No solution found!")
+    else:
+        print(solution)
