b700579701379ef6b82d06deef4ab9915dd46b7c
hspark <ssook7979@gmail.com>
2022-11-10 15:32:19 +0900

    added code for run solver

--- constraint-satisfaction-problem/word_search.py
+++ constraint-satisfaction-problem/word_search.py
@@ -44,3 +44,40 @@ def generate_domain(word: str, grid: Grid) -> List[List[GridLocation]]:
                     domain.append([GridLocation(r, col - (r - row))
                                   for r in rows])
     return domain
+
+
+class WordSearchConstraint(Constraint[str, List[GridLocation]]):
+    def __init__(self, words: List[str]) -> None:
+        super().__init__(words)
+        self.words: List[str] = words
+
+    def satisfied(self, assignment: Dict[str, List[GridLocation]]) -> bool:
+        # if there are any duplicates grid locations, then there is an overlap
+        all_locations = [locs for values in assignment.values()
+                         for locs in values]
+        return len(set(all_locations)) == len(all_locations)
+
+
+if __name__ == "__main__":
+    grid: Grid = generate_grid(9, 9)
+    words: List[str] = ["MATTHEW", "JOE", "MARY", "SARAH", "SALLY"]
+    locations: Dict[str, List[List[GridLocation]]] = {}
+    for word in words:
+        locations[word] = generate_domain(word, grid)
+    csp: CSP[str, List[GridLocation]] = CSP(words, locations)
+    csp.add_constraint(WordSearchConstraint(words))
+    solution: Optional[Dict[str, List[GridLocation]]
+                       ] = csp.backtracking_search()
+    if solution is None:
+        print("No solution found!")
+    else:
+        for word, grid_locations in solution.items():
+            # random reverse half the time
+            if choice([True, False]):
+                grid_locations.reverse()
+            for index, letter in enumerate(word):
+                (row, col) = (
+                    # TODO: fix list index out of range error
+                    grid_locations[index].row, grid_locations[index].column)
+                grid[row][col] = letter
+        display_grid(grid)
