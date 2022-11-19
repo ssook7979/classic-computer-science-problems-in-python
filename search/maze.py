775deae34167270b25ac3eb88783f1646f65b7ec
ssook7979 <ssook7979@gmail.com>
2022-11-19 23:53:56 +0900

    added enum for maze cell

--- search/maze.py
+++ search/maze.py
@@ -3,3 +3,10 @@ from typing import List, NamedTuple, Callable, Optional
 import random
 from math import sqrt
 from generic_search import dfs, bfs, node_to_path, astar, Node
+
+class Cell(str, Enum):
+    EMPTY = ""
+    BLOCKED = "X"
+    START = "S"
+    GOAL = "G"
+    PATH = "*"
