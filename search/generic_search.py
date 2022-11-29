328a9d502073edba63576e577945dc3ec8758e18
ssook7979 <ssook7979@gmail.com>
2022-11-29 23:05:02 +0900

    added Node class

--- search/generic_search.py
+++ search/generic_search.py
@@ -56,3 +56,13 @@ class Stack(Generic[T]):
     
     def __repr__(self) -> str:
         return repr(self._container)
+
+class Node(Generic[T]):
+    def __init__(self, state:T, parent: Optional[Node], cost: float = 0.0, heuristic: float = 0.0) -> None:
+        self.state: T = state
+        self.parent: Optional[Node] = parent
+        self.cost: float = cost
+        self.heuristic: float = heuristic
+
+    def __lt__(self, other: Node) -> bool:
+        return (self.cost + self.heuristic) < (other.cost + other.heuristic)
