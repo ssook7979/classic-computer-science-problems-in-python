05f599d9993f3a8827d325ddc2576687a5beffa7
ssook7979 <ssook7979@gmail.com>
2023-01-07 23:28:09 +0900

    added compress logic

--- compression/trivial_compression.py
+++ compression/trivial_compression.py
@@ -1,3 +1,18 @@
 class CompressedGene:
     def __init__(self, gene: str) -> None:
         self._compress(gene)
+    
+    def _compress(self, gene: str) -> None:
+        self.bit_string: int = 1
+        for nucleotide in gene.upper():
+            self.bit_string <<= 2
+            if nucleotide == "A":
+                self.bit_string |= 0b00
+            elif nucleotide == "C":
+                self.bit_string |= 0b01
+            elif nucleotide == "G":
+                self.bit_string |= 0b10
+            elif nucleotide == "T":
+                self.bit_string |= 0b11
+            else:
+                raise ValueError("Invalid nucleotide: {}".format(nucleotide))
