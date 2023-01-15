d12fefe4259ffcb2c20ec7e6c14dcc22195a22fb
ssook7979 <ssook7979@gmail.com>
2023-01-15 23:46:46 +0900

    added decompression logic

--- compression/trivial_compression.py
+++ compression/trivial_compression.py
@@ -19,3 +19,19 @@ class CompressedGene:
     
     def decompress(self) -> str:
         gene: str = ""
+        for i in range(0, self.bit_string.bit_length() - 1, 2):
+            bits: int = self.bit_string >> i & 0b11
+            if bits == 0b00:
+                gene += "A"
+            elif bits == 0b01:
+                gene += "C"
+            elif bits == 0b10:
+                gene += "G"
+            elif bits == 0b11:
+                gene += "T"
+            else:
+                raise ValueError("Invalid bits: {}".format(bits))
+        return gene[::-1]
+    
+    def __str__(self) -> str:
+        return self.decompress()
