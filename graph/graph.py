b9b431b5a39f7fc01aef0b68f22d1b20521b36f6
ssook7979 <ssook7979@gmail.com>
2023-03-08 23:57:45 +0900

    add graph module

---
+++ graph/graph.py
@@ -0,0 +1,9 @@
+from typing import TypeVar, Generic, List, Optional
+from edge import Edge
+
+V = TypeVar('V')
+
+class Graph(Generic[V]):
+    def __init__(self, vertices: List[V] = []) -> None:
+        self._vertices: List[V] = vertices
+        self._edges: List[List[Edge]] = [[] for _ in vertices]
