78db52330bb47c74a061912b29531a4cff9014f3
ssook7979 <ssook7979@gmail.com>
2023-03-09 23:53:02 +0900

    add properties for graph

--- graph/graph.py
+++ graph/graph.py
@@ -7,3 +7,11 @@ class Graph(Generic[V]):
     def __init__(self, vertices: List[V] = []) -> None:
         self._vertices: List[V] = vertices
         self._edges: List[List[Edge]] = [[] for _ in vertices]
+
+    @property
+    def vertex_count(self) -> int:
+        return len(self._vertices)
+    
+    @property
+    def edge_count(self) -> int:
+        return sum(map(len, self._edges))
